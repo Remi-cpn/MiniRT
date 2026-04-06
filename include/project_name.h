c1b20e1fe222c3d8cdfd01081d346b3ff0d03df8
Remi-cpn <cpnpro@outlook.fr>
2026-04-06 13:31:49 +0200

    Create branch

---
+++ include/project_name.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_NAME_H
+# define PROJECT_NAME_H
+
+# include "../libft/libft.h"
+# include "stdlib.h"
+# include "unistd.h"
+
+int	main(void);
+
+#endif // !DEBUG
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "project_name.h"
+
+int	main(void)
+{
+	write(1, "HELLO", 4);
+	return (0);
+}
