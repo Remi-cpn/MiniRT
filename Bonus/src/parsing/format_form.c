3aa7e2903f4b5926f0cf48483e9c58e28f6b743a
compain <cpnpro@outlook.fr>
2026-04-21 08:25:37 +0200

    AJout du damier cylindre et caps Bonus finit

--- Bonus/src/parsing/format_form.c
+++ Bonus/src/parsing/format_form.c
@@ -6,12 +6,30 @@
 /*   By: rcompain <rcompain@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 12:40:03 by rcompain          #+#    #+#             */
-/*   Updated: 2026/04/20 17:56:03 by rcompain         ###   ########.fr       */
+/*   Updated: 2026/04/20 21:33:49 by rcompain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 #include "../../include/parsing.h"
+
+static void	pars_chessboard(t_parsing *p, t_object *o, char *scale, char *color)
+{
+	if (double_valid(scale))
+	{
+		o->texture.type = TEX_DAM;
+		o->texture.scale = ft_atod(scale);
+		o->texture.color2 = get_color(p, color);
+		if (o->texture.scale > 0)
+			return ;
+	}
+	if (o->type == OBJ_SPHERE)
+		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_SP_ARGS_MSG);
+	if (o->type == OBJ_PLANE)
+		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_PL_ARGS_MSG);
+	if (o->type == OBJ_CYLINDER)
+		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_CY_ARGS_MSG);
+}
 
 void	add_sp(t_parsing *p, t_object *o, char **line_split)
 {
@@ -25,18 +43,11 @@ void	add_sp(t_parsing *p, t_object *o, char **line_split)
 		o->shape.sphere.radius = ft_atod(line_split[2]) / 2.0;
 		o->color = get_color(p, line_split[3]);
 		if (line_split[4])
-		{
-			if (!double_valid(line_split[4]))
-				exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_SP_ARGS_MSG);
-			o->texture.type = TEX_DAM;
-			o->texture.scale = ft_atod(line_split[4]);
-			o->texture.color2 = get_color(p, line_split[5]);
-		}
+			pars_chessboard(p, o, line_split[4], line_split[5]);
 	}
 	else
 		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_SP_ARGS_MSG);
-	if (o->shape.sphere.radius <= 0.0
-		|| (line_split[4] && o->texture.scale <= 0))
+	if (o->shape.sphere.radius <= 0.0)
 		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_SP_ARGS_MSG);
 }
 
@@ -52,24 +63,17 @@ void	add_pl(t_parsing *p, t_object *o, char **line_split)
 			exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_PL_ARGS_MSG);
 		o->color = get_color(p, line_split[3]);
 		if (line_split[4])
-		{
-			if (!double_valid(line_split[4]))
-				exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_PL_ARGS_MSG);
-			o->texture.type = TEX_DAM;
-			o->texture.scale = ft_atod(line_split[4]);
-			o->texture.color2 = get_color(p, line_split[5]);
-		}
+			pars_chessboard(p, o, line_split[4], line_split[5]);
 	}
 	else
 		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_PL_ARGS_MSG);
-	if (line_split[4] && o->texture.scale <= 0)
-		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_SP_ARGS_MSG);
 }
 
 void	add_cy(t_parsing *p, t_object *o, char **line_split)
 {
 	if (line_split[1] && line_split[2] && line_split[3] && line_split[4]
-		&& line_split[5] && !line_split[6])
+		&& line_split[5] && (!line_split[6]
+			|| (line_split[6] && line_split[7] && !line_split[8])))
 	{
 		if (!double_valid(line_split[3]) || !double_valid(line_split[4]))
 			exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_CY_ARGS_MSG);
@@ -77,10 +81,12 @@ void	add_cy(t_parsing *p, t_object *o, char **line_split)
 		o->shape.cylinder.center = get_vec(p, line_split[1]);
 		o->shape.cylinder.axis = get_vec(p, line_split[2]);
 		if (fabs(vec_norm(o->shape.cylinder.axis) - 1.0) > 0.001)
-			exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_PL_ARGS_MSG);
+			exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_CY_ARGS_MSG);
 		o->shape.cylinder.radius = ft_atod(line_split[3]) / 2.0;
 		o->shape.cylinder.height = ft_atod(line_split[4]);
 		o->color = get_color(p, line_split[5]);
+		if (line_split[6])
+			pars_chessboard(p, o, line_split[6], line_split[7]);
 	}
 	else
 		exit_prog_pars(p, ERROR_FILE_OBJ, ERROR_FILE_CY_ARGS_MSG);
