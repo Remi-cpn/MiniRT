8d7d3a84950660714c0c628d17d325cc5a46856f
Remi-cpn <cpnpro@outlook.fr>
2026-05-05 16:49:15 +0200

    Fix strncmp pars_line

--- Mandatory/src/parsing/parsing_line.c
+++ Mandatory/src/parsing/parsing_line.c
@@ -6,7 +6,7 @@
 /*   By: rcompain <rcompain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 15:49:29 by rcompain          #+#    #+#             */
-/*   Updated: 2026/04/13 12:12:34 by rcompain         ###   ########.fr       */
+/*   Updated: 2026/05/05 16:48:52 by rcompain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ void	pars_line(t_parsing *p, t_world *w, char *line)
 	p->line_split = ft_split(line, ' ');
 	if (!p->line_split || !p->line_split[0])
 		exit_prog_pars(p, ERROR_MALLOC, ERROR_MALLOC_MSG);
-	if (!ft_strncmp(p->line_split[0], "A", 1))
+	if (!ft_strncmp(p->line_split[0], "A", 2))
 		add_al(p, w, p->line_split);
-	else if (!ft_strncmp(p->line_split[0], "C", 1))
+	else if (!ft_strncmp(p->line_split[0], "C", 2))
 		add_cam(p, w, p->line_split);
-	else if (!ft_strncmp(p->line_split[0], "L", 1))
+	else if (!ft_strncmp(p->line_split[0], "L", 2))
 		add_light(p, w, p->line_split);
-	else if (!ft_strncmp(p->line_split[0], "sp", 2))
+	else if (!ft_strncmp(p->line_split[0], "sp", 3))
 		add_sp(p, &w->objects[i++], p->line_split);
-	else if (!ft_strncmp(p->line_split[0], "pl", 2))
+	else if (!ft_strncmp(p->line_split[0], "pl", 3))
 		add_pl(p, &w->objects[i++], p->line_split);
-	else if (!ft_strncmp(p->line_split[0], "cy", 2))
+	else if (!ft_strncmp(p->line_split[0], "cy", 3))
 		add_cy(p, &w->objects[i++], p->line_split);
 	else
 		exit_prog_pars(p, ERROR_FILE_ARGS, ERROR_FILE_ARGS_MSG);
